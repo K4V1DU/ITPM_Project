ba3296bbcbe7973f8069e39595d4fcea3c365a48
nimeshdilshaan <nimeshdilshan126@gmail.com>
2026-02-15 11:19:49 +0530

    update Accommondation - Nimesh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import Boardings from "./Components/Boardings/Boardings";
-
-
+import Hostpage from "./Components/HostPage/HostPage";
+import AddAccommodation from "./Components/AddAccommodation/AddAccommodation";
+import AccommodationCustom from "./Components/AccommodationCustom/AccommodationCustom";
 
 
 
@@ -15,6 +16,13 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Boardings />} />
+      <Route path="/host" element={<Hostpage />} />
+      <Route path="/add-accommodation" element={<AddAccommodation />} />
+      <Route path="/cust" element={<AccommodationCustom />} />
+
+      
+
+
     </Routes>
   );
 }
