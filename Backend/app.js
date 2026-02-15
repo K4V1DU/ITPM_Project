c94624f8f2190859f0fe37d1d9d80f434cd5fdf1
kavidu Purnamal <kavidupurnamal2001@gmail.com>
2026-02-15 13:19:28 +0530

    add user, reviews crud

--- Backend/app.js
+++ Backend/app.js
@@ -4,34 +4,68 @@ const cors = require("cors");
 const app = express();
 const multer = require("multer");
 
-// --------------------------
+
 // Middleware
-// --------------------------
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// --------------------------
+
 // Multer Setup for Photo Uploads
-// --------------------------
 const storage = multer.memoryStorage(); // store image in memory as Buffer
 const upload = multer({ storage });
 
-// --------------------------
+
+
 // Import Routes
-// --------------------------
 const AccommodationRouter = require("../Backend/Routes/Accommodation_Route");
-const PhotoRouter = require("../Backend/Routes/Photo_Route"); // your new photo routes
+const PhotoRouter = require("../Backend/Routes/Photo_Route"); 
+const ReviewRouter = require("../Backend/Routes/Review_Route"); 
+const UserRouter = require("../Backend/Routes/User_Route"); 
 
-// --------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Mount Routes
-// --------------------------
 app.use("/Accommodation", AccommodationRouter);
-app.use("/Photo", PhotoRouter); // handle single photo upload
+app.use("/Photo", PhotoRouter); 
+app.use("/Review", ReviewRouter);
+app.use("/User", UserRouter);
 
-// --------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // MongoDB Connection
-// --------------------------
 mongoose.connect(
   "mongodb+srv://K4V1DU:ekwpjA9nDZid3iqR@cluster0.23nczaf.mongodb.net/testing2?retryWrites=true&w=majority"
 )
